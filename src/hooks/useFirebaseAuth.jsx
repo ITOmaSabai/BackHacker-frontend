a1f222b77458e2e19d3dd981a081b401950bcf00
ItoSbai <lualaa.design2565@gmail.com>
2024-04-03 14:55:05 +0900

    add:Firebaseを用いたカスタムhookを追加

---
+++ src/hooks/useFirebaseAuth.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../lib/firebase";
+import {
+  onAuthStateChanged,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+export default function useFirebaseAuth() {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  const loginWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+
+    if (result) {
+      const user = result.user;
+
+      navigate("/");
+      return user;
+    }
+  };
+
+  const clear = () => {
+    setCurrentUser(null);
+    setLoading(false);
+  };
+
+  const logout = () => {
+    signOut(auth).then(clear);
+    navigate("/");
+  };
+
+  const nextOrObserver = async (user) => {
+    if (!user) {
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+    setCurrentUser(user);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, nextOrObserver);
+    return unsubscribe;
+  }, []);
+
+  return {
+    currentUser,
+    loading,
+    loginWithGoogle,
+    logout,
+  };
+}
