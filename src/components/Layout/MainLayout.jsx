42fb6fe1edac432b899c96e1b80a7d367baf5163
ItoSbai <lualaa.design2565@gmail.com>
2024-04-05 14:20:28 +0900

    update:ログイン用ボタンのデザインを修正

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -7,7 +7,7 @@ import { IconButton } from "@mui/material";
 export const MainLayout = () => {
   return (
     <>
-    <SignInButton text={"会員登録・ログイン"} />
+    <SignInButton text={"ログイン"} />
     <IconButton ><Link to={"http://localhost:3001/profile"} style={{color: "inherit", textDecoration: "none"}} target='_blank'>プロフィール</Link></IconButton>
     <WithdrawalButton />
     <TransitionsSnackbar />
