7275b6dc55c5a947ec4b6bdb8caac6a678e5ad20
ItoSbai <lualaa.design2565@gmail.com>
2024-04-06 11:34:19 +0900

    feat:マップ上のクリックした地点にマーカーを表示する機能を追加

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -1,9 +1,18 @@
 import { Box } from '@mui/material';
 import { Map, Marker } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
 
 const defaultPosition = { lat: 15.637474886767327, lng: 151.47934266767578 }
 
 export const MapView = () => {
+  const [ latLng, setLatLng ] = useState({});
+
+  const handleMapClick = (e) => {
+    const lat = parseFloat(e.detail.latLng.lat);
+    const lng = parseFloat(e.detail.latLng.lng);
+    setLatLng({lat: lat, lng: lng});
+  }
+
   return(
     <Box sx={{height: "80vh"}} >
       <Map
@@ -12,7 +21,11 @@ export const MapView = () => {
         clickableIcons={false}
         disableDefaultUI
         gestureHandling={'greedy'}
+        onClick={handleMapClick}
       >
+        {latLng &&
+          <Marker position={latLng} ></Marker>
+        }
       </Map>
     </Box>
   )
