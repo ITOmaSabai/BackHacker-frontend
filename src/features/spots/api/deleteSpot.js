90bc250065ff83e2c8ae81b3eb9bab9b6520f33e
ItoSbai <lualaa.design2565@gmail.com>
2024-04-11 20:39:48 +0900

    add:spotを削除するリクエストをAPIに送信する機能を追加

---
+++ src/features/spots/api/deleteSpot.js
@@ -0,0 +1,25 @@
+import { axios } from "../../../lib/axios";
+
+export const deleteSpot = async ( currentUser, spotId ) => {
+  const token = await currentUser?.getIdToken()
+
+  if (!token) {
+    throw new Error('No token found');
+  }
+  const config = {
+    headers: { authorization: `Bearer ${token}` },
+  };
+
+  try {
+    const res = await axios.delete(`/api/v1/spots/${spotId}`, config);
+    return res.data.message;
+  } catch (err) {
+    let message;
+    if (axios.isAxiosError(err) && err.response) {
+      console.error(err.response.data.message);
+    } else {
+      message = String(err);
+      console.error(message);
+    }
+  }
+}
