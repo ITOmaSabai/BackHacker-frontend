6085671a1413ca5cf4806115e44c97607885389a
ItoSbai <lualaa.design2565@gmail.com>
2024-04-12 14:09:30 +0900

    add:スポットのPUTリクエストをAPIに送信する機能を追加

---
+++ src/features/spots/api/updateSpot.js
@@ -0,0 +1,42 @@
+import { axios } from "../../../lib/axios";
+
+export const updateSpot = async (
+    currentUser,
+    spotName,
+    spotDescription,
+    spotLatLng,
+    address
+  ) => {
+  const token = await currentUser?.getIdToken();
+
+  if (!token) {
+    throw new Error('No token found');
+  }
+  const config = {
+    headers: { authorization: `Bearer ${token}` },
+  };
+
+  const data = {
+    spot: {
+    name: spotName,
+    description: spotDescription,
+    lat: spotLatLng.lat,
+    lng: spotLatLng.lng,
+    address_components: address.address_components,
+    formatted_address: address.formatted_address,
+    }
+  }
+
+  try {
+    const res = await axios.put("/api/v1/spots", data, config);
+    return res.data;
+  } catch (err) {
+    let message;
+    if (axios.isAxiosError(err) && err.response) {
+      console.error(err.response.data.message);
+    } else {
+      message = String(err);
+      console.error(message);
+    }
+  }
+}
