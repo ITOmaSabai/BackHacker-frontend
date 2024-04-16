6a43ff3ee20ed3bdce984a006c7fb9c4905f5e87
ItoSbai <lualaa.design2565@gmail.com>
2024-04-16 14:04:30 +0900

    feat:トップページのボタンに、スポット一覧画面へのリンクを追加

--- src/components/Layout/HeroLayout.jsx
+++ src/components/Layout/HeroLayout.jsx
@@ -1,12 +1,17 @@
 import { Box, Button, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
 
 export const HeroLayout = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/map");
+  }
   return (
     <Box sx={{height: "100%", display: "flex", alignItems: "center"}} bgcolor={"black"} >
-      <Box >
-        <Typography variant="h1" color={"white"} sx={{mx: 5, mb: 1}} >BackHacker.</Typography>
-        <Typography variant="h4" color={"white"} sx={{mx: 5, mb: 7}} >ひらけPC。いくぞ世界。</Typography>
-        <Button variant="contained" color="primary" sx={{mx: 6, mb: 3}} >旅を始める</Button>
+      <Box sx={{mx: 20}} >
+        <Typography variant="h1" color={"white"} sx={{mb: 1}} >BackHacker.</Typography>
+        <Typography variant="h4" color={"white"} sx={{mb: 7}} >ひらけPC。いくぞ世界。</Typography>
+        <Button onClick={handleClick} variant="contained" color="primary" sx={{mx: 1, mb: 3}} >旅を始める</Button>
       </Box>
     </Box>
   )
