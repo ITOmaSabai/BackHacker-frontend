4bab31fc684ec387b47d995efdbe098e9d3cf7d4
ItoSbai <lualaa.design2565@gmail.com>
2024-04-19 15:22:29 +0900

    feat:未ログイン時に新規投稿ボタンをクリックした際に、ログインを促すモーダルを表示する機能を追加

--- src/components/Layout/IndexSpotLayout.jsx
+++ src/components/Layout/IndexSpotLayout.jsx
@@ -6,13 +6,23 @@ import { useState } from "react";
 import SpotDetailModal from "../Elements/Modals/SpotDetailModal";
 import { useLocation } from "react-router-dom";
 import FloatingButton from "../Elements/Buttons/FloatingButton";
+import { useFirebaseAuth } from "../../hooks/useFirebaseAuth";
+import MessageModal from "../Elements/Modals/MessageModal";
 
 export const IndexSpotLayout = () => {
+  const { currentUser, loading } = useFirebaseAuth();
   const { spotId } = useParams();
   const navigate = useNavigate();
   const location = useLocation();
   const [ clickedMarkerId, setClickedMarkerId ] = useState(location.state?.spotId ?? false);
   const [ open, setOpen ] = useState(location.state?.open ?? false);
+  const [ loginModalOpen, setLoginModalOpen ] = useState(false);
+
+  const createSpotModalMessage = {
+    title: "ログインするとスポット投稿できます",
+    body: "街の様子を動画で楽しもう！",
+    icon: "📺 👀"
+  };
 
   const handleMarkerClick = (spotId) => {
     setClickedMarkerId(spotId);
@@ -21,7 +31,13 @@ export const IndexSpotLayout = () => {
   }
 
   const handleButtonClick = () => {
-    navigate("/spots");
+    if (loading) return;
+
+    if (!currentUser) {
+      setLoginModalOpen(true);
+    } else {
+      navigate("/spots");
+    }
   }
 
   return (
@@ -34,6 +50,14 @@ export const IndexSpotLayout = () => {
           />
           <SpotDetailModal spotId={spotId} open={open} setOpen={setOpen} />
           <FloatingButton text={"新規投稿"} onClick={handleButtonClick} />
+          <MessageModal
+            open={loginModalOpen}
+            setOpen={setLoginModalOpen}
+            title={createSpotModalMessage.title}
+            body={createSpotModalMessage.body}
+            icon={createSpotModalMessage.icon}
+            button={"login"}
+          />
         </MapView>
       </Box>
     </Box>
