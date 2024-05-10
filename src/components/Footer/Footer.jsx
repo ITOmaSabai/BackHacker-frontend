bb97fb9cf1d2929d5158af89b00b8b49195b1682
ItoSbai <lualaa.design2565@gmail.com>
2024-05-10 20:11:52 +0900

    feat:フッターのレイアウトを修正

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,11 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <Box
-      sx={{bgcolor: "primary.main", height: "2.5rem", px: 15}}
+      sx={{bgcolor: "primary.main", height: "2.5rem", px: { xs: 1, sm: 10, md: 15 }, pb: { xs: 1, sm: 0, md: 0 } }}
       display="flex"
       justifyContent="space-between"
       alignItems="center"
+      flexDirection={{ xs: "column", sm: "row", md: "row" }}
     >
       <Box display="flex" sx={{gap: 5}} >
         <Link
