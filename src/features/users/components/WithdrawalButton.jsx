37e03fe72695a97995e6f0958a34678546317da3
ItoSbai <lualaa.design2565@gmail.com>
2024-05-10 14:17:06 +0900

    fix:auth/requires-recent-loginエラー解消のため、退会前にログインする処理を追加

--- src/features/users/components/WithdrawalButton.jsx
+++ src/features/users/components/WithdrawalButton.jsx
@@ -1,20 +1,37 @@
 import { Box, Typography } from "@mui/material";
-import { deleteUser as deleteUserFromFirebase } from "firebase/auth";
+import {
+  deleteUser as deleteUserFromFirebase,
+  signInWithPopup,
+  GoogleAuthProvider,
+  reauthenticateWithCredential
+} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { deleteUser } from "../api/deleteUser";
 import { useFirebaseAuth } from "../../../hooks/useFirebaseAuth";
 import WarningIcon from '@mui/icons-material/Warning';
+import { auth } from "../../../lib/firebase";
 
 export const WithdrawalButton = () => {
-  const { currentUser, loginWithGoogle } = useFirebaseAuth();
-
+  const { currentUser } = useFirebaseAuth();
 
   const navigate = useNavigate();
 
   const withdrawalUser = async () => {
     if (!currentUser) return;
 
-    await loginWithGoogle();
+    // 退会するためには最近ログインしている必要があるので、ログイン処理を実行する
+    const login = async () => {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      return result;
+    }
+
+    // ログインしたユーザーから新しい認証情報を取得し、ユーザーを再認証する
+    // see document:https://firebase.google.com/docs/auth/web/manage-users?hl=ja#re-authenticate_a_user
+    const result = await login();
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    await reauthenticateWithCredential(currentUser, credential);
+    // Firebaseからユーザーを削除後、データベースからもユーザーを削除する
     await deleteUserFromFirebase(currentUser)
     .then(() => {
       deleteUser(currentUser).then((message) => {
